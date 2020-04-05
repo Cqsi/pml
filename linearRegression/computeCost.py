b8a2c41936f6d3034e0c26bd856c923bda1eb1aa
Casimir Rönnlöf <casimirr04@gmail.com>
2020-04-05 18:11:19 +0300

    test

--- linearRegression/computeCost.py
+++ linearRegression/computeCost.py
@@ -3,6 +3,6 @@ def computeCost(x, y, theta):
     m = len(y)
     error=0
     for i in range(m):
-        error+=(theta[0]+theta[1]*x[i]-y[i])^2
+        error+=(theta[0]+theta[1]*x[i]-y[i])**2
 
     return (1/(2*m))*error
