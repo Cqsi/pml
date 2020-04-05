2ad80d56782e04a208c50d31fa368e7039aba4e4
Casimir Rönnlöf <casimirr04@gmail.com>
2020-04-05 19:50:23 +0300

    test

--- linearRegression/main.py
+++ linearRegression/main.py
@@ -1,10 +1,14 @@
 from loadData import loadData
 from computeCost import computeCost
+from gradientDescent import gradient
 
-path = "linearRegression\data.txt"
+path = "machineLearning\\linearRegression\\data.txt"
 
 x = loadData(path)[0]
 y = loadData(path)[1]
 
-theta = [0, 1]
-print(computeCost(x, y, theta))
+theta = [0,0]
+alpha = 0.1
+iterations = 1500
+
+print(gradient(x, y, theta, alpha, iterations))
