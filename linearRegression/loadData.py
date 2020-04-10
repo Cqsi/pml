9377049456cc6d2367e4eb40bd9a320d70fa85a6
Casimir Rönnlöf <casimirr04@gmail.com>
2020-04-10 13:56:51 +0300

    test

--- linearRegression/loadData.py
+++ linearRegression/loadData.py
@@ -4,7 +4,7 @@ def loadData(path):
     with open(path) as file:
         for line in file:
             # x.append([1, int(line.split()[0])]) makes first column as 1's
-            x.append(int(line.split()[0]))
-            y.append(int(line.split()[1]))
+            x.append(float(line.split()[0]))
+            y.append(float(line.split()[1]))
     
     return [x, y]
