ca82c7742676e283f902c455a6c48e494121e021
Casimir Rönnlöf <casimirr04@gmail.com>
2020-04-22 20:14:30 +0300

    tet23

--- vectorLinearRegression/main.py
+++ vectorLinearRegression/main.py
@@ -5,17 +5,12 @@ import matplotlib.pyplot as plt
 
 from computeCost import computeCost
 from gradientDescent import gradientDescent
-from plot import plot
 
 data = pd.read_csv('data\\ex1data1.txt', header = None) #read from dataset
 X = data.iloc[:,0] # read first column
 y = data.iloc[:,1] # read second column
 m = len(y) # number of training example
 #print(data.head()) - View first few rows of the data
-
-# temporary solution for plot function idk lol
-tempX = X
-tempY = y
 
 X = X[:,np.newaxis]
 y = y[:,np.newaxis]
@@ -26,6 +21,12 @@ ones = np.ones((m,1))
 X = np.hstack((ones, X)) # adding the intercept term
 
 #print(computeCost(X, y, theta))
-theta = gradientDescent(X, y, theta, alpha, iterations)
+#theta = gradientDescent(X, y, theta, alpha, iterations)
+#print(computeCost(X, y, theta))
 
-plot(tempX, tempY, theta)
+# Plot the data
+plt.scatter(X[:,1], y)
+plt.xlabel('Population of City in 10,000s')
+plt.ylabel('Profit in $10,000s')
+plt.plot(X[:,1], np.dot(X, theta))
+plt.show()
