bde2a8a249964616d0aa1ce45ddf4bac816c16ca
Casimir Rönnlöf <casimirr04@gmail.com>
2020-04-23 15:24:13 +0300

    test

---
+++ mutlivariateLinearRegression/gradientDescent.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def gradientDescent(X, y, theta, alpha, iterations):
+    m = len(y)
+    for _ in range(iterations):
+        temp = np.dot(X, theta) - y
+        temp = np.dot(X.T, temp)
+        theta = theta - (alpha/m) * temp
+    return theta
--- mutlivariateLinearRegression/main.py
+++ mutlivariateLinearRegression/main.py
@@ -6,22 +6,23 @@ import matplotlib.pyplot as plt
 from computeCost import computeCost
 from gradientDescent import gradientDescent
 
-data = pd.read_csv('data\\ex1data1.txt', header = None) #read from dataset
-X = data.iloc[:,0] # read first column
-y = data.iloc[:,1] # read second column
-m = len(y) # number of training example
-#print(data.head()) - View first few rows of the data
+data = pd.read_csv('data\\ex1data2.txt', sep = ',', header = None)
+X = data.iloc[:,0:2] # read first two columns into X
+y = data.iloc[:,2] # read the third column into y
+m = len(y) # no. of training samples
 
-X = X[:,np.newaxis]
-y = y[:,np.newaxis]
-theta = np.zeros([2,1])
-iterations = 1500
-alpha = 0.01
+X = (X - np.mean(X))/np.std(X) # feature normalization
+
 ones = np.ones((m,1))
-X = np.hstack((ones, X)) # adding the intercept term
+X = np.hstack((ones, X))
+alpha = 0.01
+iterations = 400
+theta = np.zeros((3,1))
+y = y[:,np.newaxis] 
 
 #print(computeCost(X, y, theta))
 theta = gradientDescent(X, y, theta, alpha, iterations)
+print(theta)
 #print(computeCost(X, y, theta))
 
 # Plot the data
