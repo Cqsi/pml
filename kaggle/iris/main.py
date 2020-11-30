b64eb0b07dcb84f12f248445162a48bafc355746
Casimir Rönnlöf <casimirr04@gmail.com>
2020-11-30 18:42:13 +0200

    test

--- kaggle/iris/main.py
+++ kaggle/iris/main.py
@@ -1,14 +1,34 @@
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
+from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder
+
+import matplotlib.pyplot as plt
+
 import pandas as pd
+
+le = LabelEncoder()
 
 df = pd.read_csv("Iris.csv")
 y = df.Species
-y[y == "Iris-setosa"] = 1
-y[y == "Iris-versicolor"] = 2
-y[y == "Iris-virginica"] = 3
+X = df.drop("Species", axis=1)
 
+# turn strings into numeric values
+le.fit(y)
+y = le.transform(y)
 
-#df["Species"][df["Species"] == ""]
+#print(y.head())
+#print(X.head())
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+gnb = GaussianNB()
+fit = gnb.fit(X_train, y_train)
+
+y_pred = gnb.predict(X_test)
+print(metrics.r2_score(y_pred, y_test))
+
+disp = metrics.plot_confusion_matrix(gnb, X_test, y_test)
+disp.figure_.suptitle("Confusion Matrix")
+plt.show()
